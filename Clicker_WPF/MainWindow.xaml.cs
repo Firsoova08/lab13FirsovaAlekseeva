c035eb03c89c79baf470b8bdaae4e7c0a75c584a
Firsoova08 <foxalishca@mail.ru>
2025-11-14 19:31:55 +0300

    Add project files.

---
+++ Clicker_WPF/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Windows;
+
+namespace Clicker_WPF
+{
+  
+    public partial class MainWindow : Window
+    {
+        private int _counter = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateCounterText();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _counter++;
+            CounterText.Text = $"Счётчик: {_counter}";
+            UpdateCounterText();
+        }
+        private void UpdateCounterText()
+        {
+            CounterText.Text = $"Счётчик: {_counter}";
+        }
+    }
+}
---
+++ GameWPF/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace GameWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
+        private Random random = new Random();
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds
+                (.1);
+            timer.Tick += Timer_Tick;
+            SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e) { 
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = $"{tenthsOfSecondsElapsed / 10F:0.0s}";
+            
+            if (matchesFound == 10) {
+                timer.Stop();
+                timeTextBlock.Text += " Play again?";
+            }
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>() {
+            "🐱","🐱",
+            "🐷","🐷",
+            "🐻","🐻",
+            "🐮","🐮",
+            "🦁","🦁",
+            "🐴","🐴",
+            "🦢","🦢",
+            "🦌","🦌",
+            "🐼","🐼",
+            "🐃","🐃"
+            };
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                if (textBlock.Name != "timeTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    int index = random.Next(animalEmoji.Count);
+                    string nextEmoji = animalEmoji[index];
+                    textBlock.Text = nextEmoji;
+                    animalEmoji.RemoveAt(index);
+                }
+            }
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
+
+
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 10)
+            {
+                SetUpGame();
+            }
+        }
+    }
+}
